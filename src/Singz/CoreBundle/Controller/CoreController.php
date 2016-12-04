37a2273494176755ecbce7ca447339b65e7f4016
Benjamin Hubert <benjam.hubert@gmail.com>
2016-12-04 20:11:31 +0100

    added filter buttons

--- src/Singz/CoreBundle/Controller/CoreController.php
+++ src/Singz/CoreBundle/Controller/CoreController.php
@@ -3,18 +3,20 @@
 namespace Singz\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 
 class CoreController extends Controller
 {
+    public function indexAction() {
+        return $this->render('SingzCoreBundle:Core:comingsoon.html.twig');
+    }
+
     public function browseAction($filter) {
         $repository = $this->getDoctrine()->getManager()->getRepository('SingzSocialBundle:Publication');
 
-        $nbPub = 5;
+        $nbPub = 20;
 
         $now = new \DateTime();
-        $threedaysago = $now->sub(new \DateInterval("P3D"));
+        $thirtyDaysAgo = $now->sub(new \DateInterval("P30D"));
 
         switch($filter) {
             case 'all':
@@ -22,12 +24,12 @@ class CoreController extends Controller
                     ->select('p, COUNT(l.publication) as l_count, u.roles')
                     ->leftjoin('SingzSocialBundle:Love', 'l', 'WITH', 'p.id = l.publication')
                     ->leftjoin('SingzUserBundle:User', 'u', 'WITH', 'p.user = u.id')
-                    //->where('p.date > :tda ')
-                    //->setParameter('tda', $threedaysago)
+                    ->where('p.date > :tda ')
+                    ->setParameter('tda', $thirtyDaysAgo)
                     ->groupBy('p')
                     ->orderBy('l_count', 'DESC')
                     ->addOrderBy('p.date', 'DESC')
-                    ->setMaxResults(5)
+                    ->setMaxResults($nbPub)
                     ->getQuery();
                 break;
             case 'singzer':
@@ -35,14 +37,13 @@ class CoreController extends Controller
                     ->select('p, COUNT(l.publication) as l_count, u.roles')
                     ->leftjoin('SingzSocialBundle:Love', 'l', 'WITH', 'p.id = l.publication')
                     ->leftjoin('SingzUserBundle:User', 'u', 'WITH', 'p.user = u.id')
-                    ->where('u.roles NOT LIKE :roles')
+                    ->where('p.date > :tda AND u.roles NOT LIKE :roles')
                     ->setParameter('roles', '%"ROLE_STARZ"%')
-                    //->where('p.date > :tda ')
-                    //->setParameter('tda', $threedaysago)
+                    ->setParameter('tda', $thirtyDaysAgo)
                     ->groupBy('p')
                     ->orderBy('l_count', 'DESC')
                     ->addOrderBy('p.date', 'DESC')
-                    ->setMaxResults(5)
+                    ->setMaxResults($nbPub)
                     ->getQuery();
                 break;
             case 'starz':
@@ -50,21 +51,18 @@ class CoreController extends Controller
                     ->select('p, COUNT(l.publication) as l_count, u.roles')
                     ->leftjoin('SingzSocialBundle:Love', 'l', 'WITH', 'p.id = l.publication')
                     ->leftjoin('SingzUserBundle:User', 'u', 'WITH', 'p.user = u.id')
-                    ->where('u.roles LIKE :roles')
+                    ->where('p.date > :tda AND u.roles LIKE :roles')
                     ->setParameter('roles', '%"ROLE_STARZ"%')
-                    //->where('p.date > :tda ')
-                    //->setParameter('tda', $threedaysago)
+                    ->setParameter('tda', $thirtyDaysAgo)
                     ->groupBy('p')
                     ->orderBy('l_count', 'DESC')
                     ->addOrderBy('p.date', 'DESC')
-                    ->setMaxResults(5)
+                    ->setMaxResults($nbPub)
                     ->getQuery();
                 break;
         }
 
         $publications = $query->getResult();
-
-        dump($publications);
 
         return $this->render('SingzCoreBundle:Core:index.html.twig', array(
             "publications" => $publications
