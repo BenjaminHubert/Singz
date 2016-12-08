7f8f1d1ccf4b20ebaca3b70bfd2e4c8801955223
Thibault Lenormand <superskoller@gmail.com>
2016-12-08 17:48:07 +0100

    feed with no publication limit

--- src/Singz/CoreBundle/Controller/CoreController.php
+++ src/Singz/CoreBundle/Controller/CoreController.php
@@ -3,6 +3,7 @@
 namespace Singz\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Request;
 
 class CoreController extends Controller
 {
@@ -66,6 +67,73 @@ class CoreController extends Controller
 
         return $this->render('SingzCoreBundle:Core:index.html.twig', array(
             "publications" => $publications
+        ));
+    }
+
+    public function feedAction(){
+        $user = $this->getUser();
+
+        $em = $this->getDoctrine()->getManager();
+
+        $folRepo = $em->getRepository('SingzSocialBundle:Follow');
+        $pubRepo = $em->getRepository('SingzSocialBundle:Publication');
+        $lovRepo = $em->getRepository('SingzSocialBundle:Love');
+
+        ///// TEMPO :
+        $user = $em->getRepository('SingzUserBundle:User')->find(54);
+        /////////////
+
+        // Get user's subscriptions
+        $query = $folRepo->createQueryBuilder('f')
+            ->where('f.follower = :follower AND f.isPending = :pending')
+            ->setParameter('follower', $user)
+            ->setParameter('pending', false)
+            ->getQuery();
+        $follows = $query->getResult();
+
+        $leaders = array();
+        foreach ($follows as $f) {
+            array_push($leaders, $f->getLeader());
+        }
+
+        // Get subscriptions' publications
+        $query = $pubRepo->createQueryBuilder('p')
+            ->where('p.user in (:follows)')
+            ->setParameter('follows', $leaders)
+            ->orderBy('p.date', 'DESC')
+            ->getQuery();
+        $publications = $query->getResult();
+
+        // Get publications' threads, comments and loves
+        $threads = array();
+        $allcomments = array();
+        $loves = array();
+
+        foreach($publications as $pub){
+            $id = $pub->getId();
+            $thread = $this->container->get('fos_comment.manager.thread')->findThreadById($id);
+            if (null === $thread) {
+                $thread = $this->container->get('fos_comment.manager.thread')->createThread();
+                $thread->setId($id);
+                $thread->setPermalink($request->getUri());
+
+                // Add the thread
+                $this->container->get('fos_comment.manager.thread')->saveThread($thread);
+            }
+
+            $comments = $this->container->get('fos_comment.manager.comment')->findCommentTreeByThread($thread);
+            $love = $lovRepo->findBy(array('publication' => $id));
+
+            $threads[$id] = $thread;
+            $allcomments[$id] = $comments;
+            $loves[$id] = $love;
+        }
+
+        return $this->render('SingzCoreBundle:Core:feed.html.twig', array(
+            "publications" => $publications,
+            "threads" => $threads,
+            "comments" => $allcomments,
+            "loves" => $loves
         ));
     }
 }
