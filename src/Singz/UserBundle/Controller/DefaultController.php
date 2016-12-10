c3e658f835b9e5d3836407ff228c1d7376aa3126
Benjamin Hubert <benjam.hubert@gmail.com>
2016-12-10 18:24:46 +0100

    Profile page main functionalities

--- src/Singz/UserBundle/Controller/DefaultController.php
+++ src/Singz/UserBundle/Controller/DefaultController.php
@@ -3,11 +3,47 @@
 namespace Singz\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request, $id)
     {
-        return $this->render('SingzUserBundle:Default:index.html.twig');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $user = $em->getRepository('SingzUserBundle:User')->find($id);
+
+        $publications = $em->getRepository('SingzSocialBundle:Publication')->findBy(array('user' => $user));
+
+        // Get publications' threads, comments
+        $threads = array();
+        $allComments = array();
+
+        foreach($publications as $pub){
+            $id = $pub->getId();
+            $thread = $this->container->get('fos_comment.manager.thread')->findThreadById($id);
+            if (null === $thread) {
+                $thread = $this->container->get('fos_comment.manager.thread')->createThread();
+                $thread->setId($id);
+                $thread->setPermalink($request->getUri());
+
+                // Add the thread
+                $this->container->get('fos_comment.manager.thread')->saveThread($thread);
+            }
+
+            $comments = $this->container->get('fos_comment.manager.comment')->findCommentTreeByThread($thread);
+
+            $threads[$id] = $thread;
+            $allComments[$id] = $comments;
+        }
+
+        return $this->render('SingzUserBundle:Default:index.html.twig', array(
+                "publications" => $publications,
+                "threads" => $threads,
+                "comments" => $allComments,
+                "user" => $user
+            )
+        );
     }
 }
