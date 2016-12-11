755ac5944016f7240a88a6adf621106c48865400
Younes SADMI <younes.sadmi@gmail.com>
2016-12-11 17:24:40 +0100

    add actions such as disable/enable comment

--- src/Singz/AdminBundle/Controller/CommentController.php
+++ src/Singz/AdminBundle/Controller/CommentController.php
@@ -3,6 +3,7 @@
 namespace Singz\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class CommentController extends Controller
 {
@@ -30,5 +31,32 @@ class CommentController extends Controller
     	return $this->render('SingzAdminBundle:Comment:show.html.twig', array(
             'comment' => $comment
         ));
+    }
+    
+    public function stateAction(Request $request, $id, $state){
+    	$em = $this->getDoctrine()->getManager();
+    	$comment = $em->getRepository('Singz\SocialBundle\Entity\Comment')->find($id);
+    	if($comment == null){
+    		throw $this->createNotFoundException('Commentaire inexistant.');
+    	}
+    	
+    	$states = [0, 1]; // states available
+    	if(!in_array($comment->getState(), $states)){
+    		throw $this->createNotFoundException('Statut inconnu.');
+    	}
+    	
+    	//if confirmation done
+    	if($request->query->get('confirmed') !== null && $request->query->get('confirmed') == true){
+    		$comment->setState((int)$state);
+    		$em->persist($comment);
+    		$em->flush();
+    		$request->getSession()->getFlashBag()->add('success', 'Comment updated');
+    		return $this->redirectToRoute('singz_admin_comment_show', ['id' => $id]);
+    	}else{
+	    	return $this->render('SingzAdminBundle:Comment:state.html.twig', array(
+	    		'comment' => $comment,
+	    		'nextState' => $state
+	    	));
+    	}
     }
 }
