bcb010ab6df787d471a4d1813b75c607b7f153e1
Younes SADMI <younes.sadmi@gmail.com>
2016-12-30 16:51:46 +0100

    view page done

--- src/Singz/SocialBundle/Controller/NotificationController.php
+++ src/Singz/SocialBundle/Controller/NotificationController.php
@@ -3,13 +3,21 @@
 namespace Singz\SocialBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class NotificationController extends Controller
 {
+	/**
+	 * @Security("has_role('ROLE_USER')")
+	 */
     public function listAction()
     {
+    	$em = $this->getDoctrine()->getManager();
+    	$notifications = $em->getRepository('SingzSocialBundle:Notification')->findBy(array(
+    		'user' => $this->getUser()			
+    	));
         return $this->render('SingzSocialBundle:Notification:list.html.twig', array(
-            
+            'notifications' => $notifications
         ));
     }
 }
