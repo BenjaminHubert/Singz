55285ae263eccfe484aca330a20560ec7a75ee4c
BertrandF26 <bfreylin@gmail.com>
2017-05-30 13:41:33 +0200

    Disabled User

---
+++ src/Singz/UserBundle/Controller/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Singz\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class UserController extends Controller
+{
+    public function enabledAction($id, $state)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository('SingzUserBundle:User')->find($id);
+        if($user == null) {
+            throw $this->createNotFoundException('Utilisateur inexistant');
+        }
+        if($state == 'enabled'){
+            $user->setEnabled(true);
+        }elseif($state == 'disabled'){
+            $user->setEnabled(false);
+        }
+        $em->flush();
+        //$this->redirectToRoute('fos_user_security_logout');
+        return $this->redirectToRoute('singz_index');
+    }
+}
