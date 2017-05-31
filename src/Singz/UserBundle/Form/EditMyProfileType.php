ac9a54bf61c79692d4fdd68fcfb1b05b608bc8c3
Younes SADMI <younes.sadmi@gmail.com>
2017-05-31 21:14:05 +0200

    the user can edit his biography

--- src/Singz/UserBundle/Form/EditMyProfileType.php
+++ src/Singz/UserBundle/Form/EditMyProfileType.php
@@ -6,15 +6,21 @@ namespace Singz\UserBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class EditMyProfileType extends AbstractType
 {
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
-		$builder->add('isPrivate', CheckboxType::class ,array(
-			'label' => 'Privé',
-			'required' => false
-		));
+		$builder
+			->add('isPrivate', CheckboxType::class, array(
+				'label' => 'Privé',
+				'required' => false
+			))
+			->add('biography', TextareaType::class, array(
+				'required' => false				
+			))
+		;
 	}
 
 	public function getParent()
