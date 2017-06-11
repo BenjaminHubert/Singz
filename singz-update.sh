c6dd42d8df979175081d6e6fa3e9c225c89dc1c5
Younes SADMI <younes.sadmi@gmail.com>
2017-06-11 16:16:04 +0200

    add default database entry

--- singz-update.sh
+++ singz-update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+
 composer install --no-scripts \
      && bin/console cache:clear \
      && bin/console doctrine:schema:update --dump-sql \
      && bin/console doctrine:schema:update --force \
      && bin/console asset:install \
+     && bin/console singz:setting:create\
      && bin/console cache:clear \
