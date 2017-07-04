440f191a7a07addb66fff66006b1adac61f2f181
Younes SADMI <younes.sadmi@gmail.com>
2017-07-04 18:40:48 +0200

    browse publication newly designed

---
+++ src/Singz/SocialBundle/Resources/public/js/getPublications.js
@@ -0,0 +1,24 @@
+function getPublications(url, container, filter, offset, limit){
+	var dataToSend = {};
+	if(filter != 'undefined'){
+		dataToSend['filter'] = filter;
+	}
+	if(offset != 'undefined'){
+		dataToSend['offset'] = offset;
+	}
+	if(limit != 'undefined'){
+		dataToSend['limit'] = limit;
+	}
+	
+	$.ajax({
+        url: url,
+        method: 'GET',
+        data: dataToSend,
+        dataType: 'JSON'
+    }).done(function(data, textStatus, jqXHR){
+        container.append(data.html);
+    }).fail(function(jqXHR, textStatus, errorThrown){
+        console.log(errorThrown);
+        toastr.error(errorThrown)
+    });
+}
