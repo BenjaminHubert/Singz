8906cd84a9bba4608b65c8f98ad1cb01b9bd2a93
Benjamin Hubert <benjam.hubert@gmail.com>
2017-07-09 11:47:37 +0200

    removed dumps

--- src/Singz/CoreBundle/Controller/CoreController.php
+++ src/Singz/CoreBundle/Controller/CoreController.php
@@ -79,8 +79,6 @@ class CoreController extends Controller
         $em = $this->getDoctrine()->getManager();
         $publications = $em->getRepository('SingzSocialBundle:Publication')->getPublicationByHashtag($user, $hashtag);
         $projects = $em->getRepository('SingzCoreBundle:Project')->getProjectByHashtag($hashtag);
-        dump($projects);
-        dump($publications);
         return $this->render('SingzCoreBundle:Core:hashtag.html.twig', array(
             "publications" => $publications,
             "projects" => $projects,
